55ce885266d43b90b6bf901be4f50e89a33a2ede
mdt835178-cmd <mdt835178@gmail.com>
2026-08-17 02:02:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function showLove(){document.getElementById("popup").classList.add("show")}function closeLove(){document.getElementById("popup").classList.remove("show")}document.getElementById("popup").addEventListener("click",function(e){if(e.target===this)closeLove()});
